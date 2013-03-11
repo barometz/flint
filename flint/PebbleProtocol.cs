64b4475146f681dc4253e607e99057f3b3461eac
barometz <dominic@baudvine.net>
2013-03-11 18:14:21 +0100

    Groundwork for the library
    The icon is blatantly stolen from r/pebble flair and probably temporary.

---
+++ flint/PebbleProtocol.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace flint
+{
+    /// <summary>
+    /// Handles the basic protocol structure for Pebble communication, turning 
+    /// the input stream into events for the various endpoints.
+    /// </summary>
+    internal class PebbleProtocol
+    {
+    }
+}
---
+++ flint/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("flint")]
+[assembly: AssemblyDescription("A .NET library for talking to Pebble smartwatches")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("flint")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("86b4aa7f-dd8b-4254-be97-da44854e68d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
