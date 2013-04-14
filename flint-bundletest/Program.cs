f828a8c0af540ac463d7da002744cbb40bfb691f
barometz <dominic@baudvine.net>
2013-04-14 17:14:58 +0200

    Add basic .pbw loading.
    Note: switched to .NET4.5 in order to take advantage of the new fancy
    ZipArchive class.

---
+++ flint-bundletest/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using flint;
+
+namespace flint_bundletest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bundle = new PebbleBundle("demo.pbw");
+            Console.WriteLine(bundle.Application);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ flint/PebbleBundle.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace flint
+{
+    /// <summary> Represents a Pebble app bundle (.pbw file). </summary>
+    public class PebbleBundle
+    {
+        /// <summary> Maps to the information in manifest.json. </summary>
+        [DataContract]
+        public class BundleManifest
+        {
+            /// <summary> Maps to the application-specific information in manifest.json. </summary>
+            [DataContract]
+            public class ApplicationManifest
+            {
+                /// <summary> The firmware version required to run this application. </summary>
+                [DataMember(Name = "reqFwVer")]
+                public readonly int RequiredFirmwareVersion;
+                /// <summary> The filename of the application binary in the bundle. </summary>
+                [DataMember(Name = "name")]
+                public readonly String Filename;
+            }
+            [DataMember(Name = "manifestVersion")]
+            public readonly int ManifestVersion;
+            [DataMember(Name="generatedAt")]
+            private int GeneratedAtTS;
+            /// <summary> The date and time at which this bundle was generated. </summary>
+            public DateTime GeneratedAt { get { return Pebble.TimestampToDateTime(GeneratedAtTS); } }
+            /// <summary> Name of the machine on which this bundle was generated. </summary>
+            [DataMember(Name = "generatedBy")]
+            public readonly String GeneratedBy;
+            /// <summary> The manifest for the application contained in this bundle. </summary>
+            [DataMember(Name = "application")]
+            public readonly ApplicationManifest Application;
+        }
+
+        /// <summary> Maps to the metadata as extracted from the application binary. </summary>
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack=1)]
+        public struct ApplicationMetadata
+        {
+            /// <summary> Gets a string representation of the application version. </summary>
+            public String AppVersion { get { return String.Format("{0}.{1}", AppMajorVersion, AppMinorVersion); } }
+            /// <summary> Gets a string representation of the SDK version used to produce this application. </summary>
+            public String SDKVersion { get { return String.Format("{0}.{1}", SDKMajorVersion, SDKMinorVersion); } }
+            /// <summary> Gets a string representation of the metadata version. </summary>
+            public String StructVersion { get { return String.Format("{0}.{1}", StructMajorVersion, StructMinorVersion); } }
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst=8)]
+            public readonly String header;
+            [MarshalAs(UnmanagedType.U1)]
+            public readonly byte StructMajorVersion;
+            [MarshalAs(UnmanagedType.U1)]
+            public readonly byte StructMinorVersion;
+            [MarshalAs(UnmanagedType.U1)]
+            public readonly byte SDKMajorVersion;
+            [MarshalAs(UnmanagedType.U1)]
+            public readonly byte SDKMinorVersion;
+            [MarshalAs(UnmanagedType.U1)]
+            public readonly byte AppMajorVersion;
+            [MarshalAs(UnmanagedType.U1)]
+            public readonly byte AppMinorVersion;
+            [MarshalAs(UnmanagedType.U2)]
+            public readonly UInt16 Size;
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly uint Offset;
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly uint CRC;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public readonly String AppName;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+            public readonly String CompanyName;
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly uint IconResourceID;
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly uint SymbolTableAddress;
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly uint Flags;
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly uint RelocationListStart;
+            [MarshalAs(UnmanagedType.U4)]
+            public readonly uint RelocationListItemCount;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 16)]
+            public readonly String UUID;
+
+            public override string ToString()
+            {
+                return String.Format("{0}, version {1}.{2} by {3}", AppName, AppMajorVersion, AppMinorVersion, CompanyName);
+            }
+        }
+
+        public String Filename { get { return Path.GetFileName(FullPath); } }
+        public String FullPath { get; private set; }
+        public ApplicationMetadata Application { get; private set; }
+        
+        ZipArchive Archive;
+        BundleManifest Manifest;
+        
+        public PebbleBundle(String path)
+        {
+            FullPath = path;
+            FileStream stream = new FileStream(path, FileMode.Open);
+            Archive = new ZipArchive(stream, ZipArchiveMode.Read);
+            ZipArchiveEntry jsonentry = Archive.GetEntry("manifest.json");
+            if (jsonentry == null)
+            {
+                throw new ArgumentException("manifest.json not found");
+            }
+            var serializer = new DataContractJsonSerializer(typeof(BundleManifest));
+            Manifest = serializer.ReadObject(jsonentry.Open()) as BundleManifest;
+            ZipArchiveEntry binentry = Archive.GetEntry(Manifest.Application.Filename);
+            if (binentry == null)
+            {
+                throw new ArgumentException(String.Format("App file {0} not found", Manifest.Application.Filename));
+            }
+            ReadApplicationMetadata(binentry.Open());
+        }
+
+        private void ReadApplicationMetadata(Stream fs)
+        {
+            // Borrowed from http://stackoverflow.com/a/1936208 because BitConverter-ing all of this would be a pain
+            byte[] buffer = new byte[Marshal.SizeOf(typeof(ApplicationMetadata))];
+            fs.Read(buffer, 0, buffer.Length);
+            GCHandle hdl = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+            try
+            {
+                Application = (ApplicationMetadata)Marshal.PtrToStructure(hdl.AddrOfPinnedObject(), typeof(ApplicationMetadata));
+            }
+            finally
+            {
+                hdl.Free();
+            }
+        }
+    }
+}
